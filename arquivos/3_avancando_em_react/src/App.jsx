a0b81a670612b793ea7c76de52385563f033d84c
Jonata Caetano <jonatacaetano88@gmail.com>
2024-03-17 20:48:30 -0300

    Rendering list in component and Fragments

--- arquivos/3_avancando_em_react/src/App.jsx
+++ arquivos/3_avancando_em_react/src/App.jsx
@@ -5,14 +5,20 @@ import ListRender from "./components/ListRender";
 import ManageData from "./components/ManageData";
 import ShowUserName from "./components/ShowUserName";
 import CarDetails from "./components/CarDetails";
+import Fragment from "./components/Fragment";
 
 function App() {
   const name = "Jonata";
   const [userName] = useState("Maria");
+
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Amarelo", newCar: true, km: 0 },
+    { id: 2, brand: "Kia", color: "Branco", newCar: false, km: 344 },
+    { id: 3, brand: "Renault", color: "Azul", newCar: false, km: 234 },
+  ];
   return (
     <div className="App">
       <h1>Avançando em React</h1>
-
       {/* Imagem no diretório public */}
       <div>
         <img
@@ -21,7 +27,6 @@ function App() {
           style={{ maxWidth: "20%", height: "auto%" }}
         />
       </div>
-
       {/* Imagem no diretório de assets */}
       <div>
         <img
@@ -37,10 +42,21 @@ function App() {
       <ShowUserName name={userName} />
       <ShowUserName name={name} />
       {/*destructuring*/}
-      <CarDetails brand="VW" km={1000} color="Azul" newCar={false}/>
+      <CarDetails brand="VW" km={1000} color="Azul" newCar={false} />
       {/*Reusing the component*/}
       <CarDetails brand="Ford" km={1000} color="Vermelho" newCar={true} />
-      <CarDetails brand="Fiat" km={0} color="Branco" newCar={false}/>
+      <CarDetails brand="Fiat" km={0} color="Branco" newCar={false} />
+      {/*Loop em array de objeto*/}
+      {cars.map((car) => (
+        <CarDetails
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
+      {/*Fragment*/}
+      <Fragment propFragment = "teste" />
     </div>
   );
 }

9e0a305ef07b6f73b632faa40e8e7b3fb0596d82
Jonata Caetano <jonatacaetano88@gmail.com>
2024-03-18 23:27:15 -0300

    Passing function as prop and prop children

--- arquivos/3_avancando_em_react/src/App.jsx
+++ arquivos/3_avancando_em_react/src/App.jsx
@@ -6,6 +6,8 @@ import ManageData from "./components/ManageData";
 import ShowUserName from "./components/ShowUserName";
 import CarDetails from "./components/CarDetails";
 import Fragment from "./components/Fragment";
+import Container from "./components/Container";
+import ExecutionFunction from "./components/ExecutionFunction";
 
 function App() {
   const name = "Jonata";
@@ -16,6 +18,11 @@ function App() {
     { id: 2, brand: "Kia", color: "Branco", newCar: false, km: 344 },
     { id: 3, brand: "Renault", color: "Azul", newCar: false, km: 234 },
   ];
+
+  function showMessage() {
+    console.log("Evento do componente pai!");
+  }
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -49,6 +56,7 @@ function App() {
       {/*Loop em array de objeto*/}
       {cars.map((car) => (
         <CarDetails
+          key={car.id}
           brand={car.brand}
           color={car.color}
           km={car.km}
@@ -56,7 +64,16 @@ function App() {
         />
       ))}
       {/*Fragment*/}
-      <Fragment propFragment = "teste" />
+      <Fragment propFragment="teste" />
+      {/*Children*/}
+      <Container>
+        <p>E este é o conteudo</p>
+      </Container>
+      <Container myValeu="testing 2">
+        <h5>Testando o container</h5>
+      </Container>
+      {/*Executar função*/}
+      <ExecutionFunction myFunction={showMessage} />
     </div>
   );
 }

08340db14941bf1f904af73405daf808ba6437c1
Jonata Caetano <jonatacaetano88@gmail.com>
2024-03-21 21:50:29 -0300

    Dynamic Inline CSS and Dynamic Classes

--- arquivos/4_css_react/css/src/App.jsx
+++ arquivos/4_css_react/css/src/App.jsx
@@ -1,8 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MyComponent from "./components/MyComponent";
+import { useState } from "react";
 
 function App() {
+  const n = 15;
+  const [name] = useState("Matheus");
+  const redTitle = false;
   return (
     <div className="App">
       {/*CSS de component*/}
@@ -10,9 +14,35 @@ function App() {
       <MyComponent />
       <p>Este parágrafo é do App.js</p>
       {/*Inline CSS*/}
-      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+      <p
+        style={{
+          color: "blue",
+          padding: "25px",
+          borderTop: "2px solid red",
+        }}
+      >
         Este elemento foi estilizado de forma inline
       </p>
+      {/*CSS Inline dinâmico*/}
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinamico
+      </h2>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinamico
+      </h2>
+      <h2
+        style={
+          name === "Matheus"
+            ? { color: "green", backgroundColor: "#000" }
+            : null
+        }
+      >
+        Teste nome
+      </h2>
+      {/*Classe dinânica*/}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este título vai ter classe dinâmica
+      </h2>
     </div>
   );
 }

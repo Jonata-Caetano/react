df9afc6edc861fc658592c30d42917f4f91b5d13
Jonata Caetano <jonatacaetano88@gmail.com>
2024-03-26 21:18:02 -0300

    Select

--- arquivos/5_form_em_react/src/App.jsx
+++ arquivos/5_form_em_react/src/App.jsx
@@ -5,7 +5,14 @@ function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm user={{ name: "Josias", email: "josias@hotmail.com" }} />
+      <MyForm
+        user={{
+          name: "Josias",
+          email: "josias@hotmail.com",
+          bio: "Sou um advogado",
+          role: "admin",
+        }}
+      />
     </div>
   );
 }
--- arquivos/5_form_em_react/src/Components/MyForm.jsx
+++ arquivos/5_form_em_react/src/Components/MyForm.jsx
@@ -6,7 +6,8 @@ const MyForm = ({ user }) => {
   // 3 - Gerencinamento de dados
   const [name, setName] = useState(user ? user.name : "");
   const [email, setEmail] = useState(user ? user.email : "");
-  const [bio, setBio] = useState("");
+  const [bio, setBio] = useState(user ? user.bio : "");
+  const [role, setRole] = useState(user ? user.role : "");
 
   const handleName = (e) => {
     setName(e.target.value);
@@ -15,7 +16,7 @@ const MyForm = ({ user }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Enviando formulario");
-    console.log(name, email, bio);
+    console.log(name, email, bio, role);
 
     // 7 - Limpar formulario
     setName("");
@@ -61,6 +62,19 @@ const MyForm = ({ user }) => {
             onChange={(e) => setBio(e.target.value)}
             value={bio}
           ></textarea>
+        </label>
+        {/*9 -Select*/}
+        <label>
+          <span>Função no sistema</span>
+          <select
+            name="role"
+            onChange={(e) => setRole(e.target.value)}
+            value={role}
+          >
+            <option value="user">Usuário</option>
+            <option value="editor">Editor</option>
+            <option value="admin">Admin</option>
+          </select>
         </label>
         {/*8 - textarea*/}
         <input type="submit" value="Enviar" />

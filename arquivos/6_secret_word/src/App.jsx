14862c0af5139279ccf1405ba830b3baa2be3abd
Jonata Caetano <jonatacaetano88@gmail.com>
2024-03-30 22:15:14 -0300

    ndows@DESKTOP-RA8U7TQ MINGW64 /j/Jônata/Github/react (main)

---
+++ arquivos/6_secret_word/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+// Components
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+// Styles
+import "./App.css";
+
+// Data
+import { wordsList } from "./data/words";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  console.log(words);
+
+  const pickWordAndCategory = () => {
+    // Pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // Pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(category, word);
+
+    return { word, category };
+  };
+
+  // Start the game
+  const startGame = () => {
+    // Choose a word
+    const { category, word } = pickWordAndCategory();
+
+    // Create an array of letters
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(category, word);
+    console.log(wordLetters);
+
+    // Fill States
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  };
+
+  // Process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
+  return (
+    <div className="App">
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
+    </div>
+  );
+}
+
+export default App;
--- arquivos/6_secret_word/src/components/StartScreen.jsx
+++ arquivos/6_secret_word/src/components/StartScreen.jsx
@@ -1,7 +1,10 @@
 import "./StartScreen.css";
 
 const StartScreen = ({ startGame }) => {
+
+
   return (
+
     <div className="start">
       <h1>Secret Word</h1>
       <p>Clique no botão para começar a jogar</p>

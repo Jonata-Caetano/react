47f5ef487a25e3fba7842dd5e01bef20fc55107e
Jonata Caetano <jonatacaetano88@gmail.com>
2024-04-29 23:29:24 -0300

    Dynamic routes

--- arquivos/8_react_router/src/pages/Home.jsx
+++ arquivos/8_react_router/src/pages/Home.jsx
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom";
-import { useFetch } from "../hooks/use-fetch";
+import { useFetch } from "../hooks/useFetch";
+
+import "./Home.css";
 
 const Home = () => {
-  // carremento de dados
+  // 3 - carregamento de dados
   const url = "http://localhost:3000/products";
+
   const { data: items, loading, error } = useFetch(url);
 
   return (
     <div>
       <h1>Produtos</h1>
+      {loading && <p>Carregando dados...</p>}
       {error && <p>{error}</p>}
       <ul className="products">
         {items &&
-          items.map((item) => (
-            <li key={item.id}>
-              <h2>{item.name}</h2>
-              <p>R$: {item.price}</p>
+          items.map((product) => (
+            <li key={product.id}>
+              <h2>{product.name}</h2>
+              <p>R$: {product.price}</p>
+              {/* 4  - rota dinamica */}
+              <Link to={`/products/${product.id}`}>Detalhes</Link>
             </li>
           ))}
       </ul>
